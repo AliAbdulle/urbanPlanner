e881bce8821f996407b39dc8027073f51577ebf5
AliAbdulle <alidhobey@gmail.com>
2019-05-29 15:23:09 -0500

    Saved all the building work for the section of the day

---
+++ Building.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace urbanPlanner
+{
+    class Building
+    {
+        private string _designer {get; set;}
+        private DateTime _dateConstructed {get; set;}
+        private string _address {get; set;}
+        private string _owner {get; set;}
+        public int Stories {get; set;}
+        public double Width {get; set;}
+        public double Depth {get; set;}
+        public double Valume 
+        {
+            get {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+        public Building (string address){
+            _address = address;
+            _designer = "Ali Adulle";
+
+        }
+        public void construct() {
+            _dateConstructed = DateTime.Now;
+        }
+        public void Purchase(string purchaser) {
+            _owner = purchaser;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace urbanPlanner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
         }
     }
 }
